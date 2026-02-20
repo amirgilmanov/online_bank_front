efcaca86930b991e66e9b546e59a6d06a78252c9
gilmi19 <gilmanovamir19@gmail.com>
2026-02-20 23:02:27 +0500

    bite standard env named

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const REACT_APP_API_URL = process.env.BACKEND_URL
+const REACT_APP_API_URL = process.env.REACT_APP_API_URL
 
 const api = axios.create({
     baseURL: REACT_APP_API_URL,
